f84472df4c4e8bf94d645d5b5bbcba9a3b62ec00
diallo3 <diallothree@gmail.com>
2020-03-13 23:00:46 -0400

    reorganized main structure of theme components. Adding tailwind ui and alpinejs

--- assets/js/components/HeaderComponent.js
+++ assets/js/components/HeaderComponent.js
@@ -1,13 +1,11 @@
 import HeadroomComponent from "./header/Headroom";
 import MmenuComponent from "./header/Mmenu";
-import MidnightJSHeadComponent from "./header/Midnight";
 
 const HeaderComponent = {
 
     init() {
         HeadroomComponent.init();
         MmenuComponent.init();
-        MidnightJSHeadComponent.init();
     }
 
 }
--- tailwind.js
+++ tailwind.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
     theme: {
         screeens: {
@@ -7,43 +9,19 @@ module.exports = {
             xl: '1280px'
         },
         extend: {
-            colors: {
-                link: {
-                    'default': '#3182ce',
-                    'hover': '#63b3ed',
-                }
-            },
-            fontSize: {
-                xxs: '0.675rem',
-                '7xl': '5rem',
-                '8xl': '6rem',
-                '9xl': '7rem',
-            },
-            lineHeight: {
-                tighter: '1.125',
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
             },
         },
-        minHeight: {
-            '1/4': '25vh',
-            '1/2': '50vh',
-            '3/4': '75vh'
-        },
+
     },
     variants: {
         textColor: ['responsive', 'hover', 'focus', 'visited'],
         display: ['responsive']
     },
     plugins: [
-        ({ addUtilities }) => {
-            const utils = {
-                '.translate-x-half': {
-                    transform: 'translateX(50%)',
-                },
-            };
-            addUtilities(utils, ['responsive'])
-        },
-        require('tailwind-bootstrap-grid')(),
-        require('@tailwindcss/custom-forms')
+        require('tailwind-bootstrap-grid'),
+        require('@tailwindcss/ui')
     ],
     corePlugins: {
         container: false
